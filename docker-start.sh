ecb4fb0b4f75e0096791f82db31059973a9ce9b0
Jimmy Huang <jimmy@netivism.com.tw>
2015-04-28 22:53:27 +0800

    Add docker for start-up script

--- docker-start.sh
+++ docker-start.sh
@@ -1,7 +1,9 @@
 WORKDIR=`pwd`
+docker stop neticrm-ci
+docker rm neticrm-ci
 docker run -d --name neticrm-ci \
   -p 8888:8888 \
   -v /etc/localtime:/etc/localtime:ro \
   -v $WORKDIR/init.sh:/init.sh \
   -e "TZ=Asia/Taipei" \
-  -i -t netivism/neticrm-ci
+  -i -t netivism/neticrm-ci /init.sh
--- init.sh
+++ init.sh
@@ -9,6 +9,8 @@ DIR="neticrm-${NETICRM}"
 
 echo "Starting MySQL server..."
 /usr/bin/mysqld_safe >/dev/null 2>&1 &
+sleep 5s
+echo "Install new database $DB"
 mysql -uroot -e "CREATE DATABASE $DB CHARACTER SET utf8 COLLATE utf8_general_ci;"
 mysql -uroot -e "CREATE USER '$DB'@'%' IDENTIFIED BY '$PW';"
 mysql -uroot -e "GRANT ALL PRIVILEGES ON $DB.* TO '$DB'@'%' WITH GRANT OPTION;"
@@ -16,15 +18,22 @@ mysql -uroot -e "FLUSH PRIVILEGES;"
 
 cd /home
 
-# install drupal
-php -d sendmail_path=`which true` ~/.composer/vendor/bin/drush.php --yes core-quick-drupal --core=drupal-${DRUPAL} --no-server --db-url=mysql://${DB}:${PW}@127.0.0.1/${DB} --account-pass=123456 --site-name=netiCRM --enable=transliteration ${DIR}
+echo "Install Drupal ..."
+sleep 5s
+php -d sendmail_path=`which true` ~/composer/vendor/bin/drush.php --yes core-quick-drupal --core=drupal-${DRUPAL} --no-server --db-url=mysql://${DB}:${PW}@127.0.0.1/${DB} --account-pass=123456 --site-name=netiCRM --enable=transliteration ${DIR}
 
-# install crm
+echo "Install netiCRM ..."
+sleep 5s
 cd ${DIR}/drupal-${DRUPAL}/sites/all/modules
 git clone --depth=50 --branch=2.0-dev git://github.com/NETivism/netiCRM.git civicrm
+cd civicrm
 git submodule init
-git submodule update --remote
-php ~/.composer/vendor/bin/drush.php --yes --debug pm-enable civicrm
+git submodule update
+php ~/composer/vendor/bin/drush.php --yes pm-enable civicrm
 
 # start php
-php ~/.composer/vendor/bin/drush.php runserver 127.0.0.1:8888 >> /dev/null &
+echo "Startup php server at $PORT ..."
+sleep 5s
+php ~/composer/vendor/bin/drush.php runserver 127.0.0.1:$PORT >> /dev/null &
+
+/bin/bash
