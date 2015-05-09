f8cc7ed1ba2bdc3ae5940d4ee57f4390e6baec5f
Jimmy Huang <jimmy@netivism.com.tw>
2015-05-09 20:17:13 +0800

    refs #15479, remove stdin and tty

--- docker-start.sh
+++ docker-start.sh
@@ -2,7 +2,7 @@ WORKDIR=`pwd`
 
 echo "Updating repository netivism/neticrm-ci"
 docker pull netivism/neticrm-ci:7.x
-docker run -it --rm \
+docker run --rm \
   --name neticrm-ci \
   -p 8888:80 \
   -v /etc/localtime:/etc/localtime:ro \
