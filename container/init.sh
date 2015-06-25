6e3feeb933302ddc4ec788a085de80be47d6c8d8
Jimmy Huang <jimmy@netivism.com.tw>
2015-06-25 12:58:20 +0800

    Add another date format

--- container/init.sh
+++ container/init.sh
@@ -9,8 +9,8 @@ DB=neticrmci
 PW=123456
 PORT=80
 
+date +"@ %Y-%m-%d %H:%M:%S %z"
 echo "CI for Drupal-$DRUPAL + netiCRM-$NETICRM"
-date +"--start %Y-%m-%d %H:%M:%S %z"
 
 echo "Install new database $DB"
 mysql -uroot -e "CREATE DATABASE $DB CHARACTER SET utf8 COLLATE utf8_general_ci;"
@@ -21,14 +21,14 @@ mysql -uroot -e "FLUSH PRIVILEGES;"
 cd $BASE
 
 echo "Install Drupal ..."
-date +"-- %Y-%m-%d %H:%M:%S %z"
+date +"@ %Y-%m-%d %H:%M:%S %z"
 sleep 5s
 php -d sendmail_path=`which true` ~/.composer/vendor/bin/drush.php --yes core-quick-drupal --core=drupal-${DRUPAL} --no-server --db-url=mysql://${DB}:${PW}@127.0.0.1/${DB} --account-pass=123456 --site-name=netiCRM --enable=transliteration neticrm_build
 mv $BASE/neticrm_build/drupal-${DRUPAL}/* $BASE/html/
 mv $BASE/neticrm_build/drupal-${DRUPAL}/.htaccess $BASE/html/
 
 echo "Install netiCRM ..."
-date +"-- %Y-%m-%d %H:%M:%S %z"
+date +"@ %Y-%m-%d %H:%M:%S %z"
 cat $BASE/html/ci.log | ansi2html --bg=dark > $BASE/html/ci.html
 
 sleep 5s
@@ -47,7 +47,7 @@ cat $BASE/html/ci.log | ansi2html --bg=dark > $BASE/html/ci.html
 
 # headless browser testing..
 echo "Headless testing"
-date +"-- %Y-%m-%d %H:%M:%S %z"
+date +"@ %Y-%m-%d %H:%M:%S %z"
 sleep 10s
 cd $BASE/html
 casperjs test sites/all/modules/civicrm/tests/casperjs/pages.js
@@ -57,7 +57,7 @@ cat $BASE/html/ci.log | ansi2html --bg=dark > $BASE/html/ci.html
 
 # phpunit 
 echo "Unit testing"
-date +"-- %Y-%m-%d %H:%M:%S %z"
+date +"@ %Y-%m-%d %H:%M:%S %z"
 cd $BASE/html/sites/all/modules/civicrm/tests/phpunit
 drush en civicrm_allpay --yes
 drush en civicrm_demo --yes
@@ -65,5 +65,5 @@ export DRUPAL_ROOT=/var/www/html
 export CIVICRM_TEST_DSN=mysql://root@127.0.0.1/neticrmci
 phpunit --colors=always CRM/Core/Payment/ALLPAYTest.php
 
-date +"--end %Y-%m-%d %H:%M:%S %z"
+date +"@ %Y-%m-%d %H:%M:%S %z"
 cat $BASE/html/ci.log | ansi2html --bg=dark > $BASE/html/ci.html
