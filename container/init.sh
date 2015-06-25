219416f98aa338e43757cd2be20dc873cc79a499
Jimmy Huang <jimmy@netivism.com.tw>
2015-06-25 12:52:38 +0800

    Add datetime tag.

--- container/init.sh
+++ container/init.sh
@@ -10,6 +10,7 @@ PW=123456
 PORT=80
 
 echo "CI for Drupal-$DRUPAL + netiCRM-$NETICRM"
+date +"--start %Y-%m-%d %H:%M:%S %z"
 
 echo "Install new database $DB"
 mysql -uroot -e "CREATE DATABASE $DB CHARACTER SET utf8 COLLATE utf8_general_ci;"
@@ -20,12 +21,14 @@ mysql -uroot -e "FLUSH PRIVILEGES;"
 cd $BASE
 
 echo "Install Drupal ..."
+date +"-- %Y-%m-%d %H:%M:%S %z"
 sleep 5s
 php -d sendmail_path=`which true` ~/.composer/vendor/bin/drush.php --yes core-quick-drupal --core=drupal-${DRUPAL} --no-server --db-url=mysql://${DB}:${PW}@127.0.0.1/${DB} --account-pass=123456 --site-name=netiCRM --enable=transliteration neticrm_build
 mv $BASE/neticrm_build/drupal-${DRUPAL}/* $BASE/html/
 mv $BASE/neticrm_build/drupal-${DRUPAL}/.htaccess $BASE/html/
 
 echo "Install netiCRM ..."
+date +"-- %Y-%m-%d %H:%M:%S %z"
 cat $BASE/html/ci.log | ansi2html --bg=dark > $BASE/html/ci.html
 
 sleep 5s
@@ -44,6 +47,7 @@ cat $BASE/html/ci.log | ansi2html --bg=dark > $BASE/html/ci.html
 
 # headless browser testing..
 echo "Headless testing"
+date +"-- %Y-%m-%d %H:%M:%S %z"
 sleep 10s
 cd $BASE/html
 casperjs test sites/all/modules/civicrm/tests/casperjs/pages.js
@@ -53,11 +57,13 @@ cat $BASE/html/ci.log | ansi2html --bg=dark > $BASE/html/ci.html
 
 # phpunit 
 echo "Unit testing"
+date +"-- %Y-%m-%d %H:%M:%S %z"
 cd $BASE/html/sites/all/modules/civicrm/tests/phpunit
 drush en civicrm_allpay --yes
 drush en civicrm_demo --yes
 export DRUPAL_ROOT=/var/www/html
 export CIVICRM_TEST_DSN=mysql://root@127.0.0.1/neticrmci
-phpunit --colors CRM/Core/Payment/ALLPAYTest.php
+phpunit --colors=always CRM/Core/Payment/ALLPAYTest.php
 
+date +"--end %Y-%m-%d %H:%M:%S %z"
 cat $BASE/html/ci.log | ansi2html --bg=dark > $BASE/html/ci.html
