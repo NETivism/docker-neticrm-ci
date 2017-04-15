cd545a980ed7a2563ffdad98bfe43337f10684c0
Jimmy Huang <jimmy@netivism.com.tw>
2017-04-15 19:23:55 +0800

    Upgrade to php-cgi to save memory

--- Dockerfile
+++ Dockerfile
@@ -11,6 +11,7 @@ RUN \
   apt-get update && \
   apt-get install -y \
     net-tools \
+    php5-cgi \
     gawk && \
   composer global require phpunit/phpunit:4.6 && \
   composer global require phpunit/dbunit
@@ -28,10 +29,23 @@ RUN \
   ln -s /srv/var/casperjs/bin/casperjs /usr/bin/casperjs && \
   apt-get autoremove -y && \
   apt-get clean && \
-  rm -rf /var/lib/apt/lists/* && \
+  rm -rf /var/lib/apt/lists/*
+
+RUN \
+  wget -q --no-check-certificate -O /tmp/drupal.tar.gz https://ftp.drupal.org/files/projects/drupal-7.54.tar.gz && \
+  tar -zxf /tmp/drupal.tar.gz -C /tmp && \
+  mv /tmp/drupal-7.54/* /var/www/html && \
+  mkdir -p /var/www/html/sites/all/modules && \
   mkdir -p /var/www/html/log/supervisor
 
+RUN \
+  wget -q --no-check-certificate -O /tmp/transliteration.tar.gz https://ftp.drupal.org/files/projects/transliteration-7.x-3.2.tar.gz && \
+  tar -zxf /tmp/transliteration.tar.gz -C /var/www/html/sites/all/modules/ && \
+  wget -q --no-check-certificate -O /tmp/simpletest.tar.gz https://ftp.drupal.org/files/projects/simpletest-7.x-2.0.tar.gz && \
+  tar -zxf /tmp/simpletest.tar.gz -C /var/www/html/sites/all/modules/
+
 ADD container/init.sh /init.sh
+ADD container/my.cnf /etc/mysql/my.cnf
 ADD container/supervisord.conf /etc/supervisor/conf.d/supervisord.conf
 ADD container/ansi2html.sh /usr/local/bin/ansi2html
 
--- container/init.sh
+++ container/init.sh
@@ -3,18 +3,16 @@
 while ! pgrep -u mysql mysqld > /dev/null; do sleep 3; done
 
 REPOSDIR=`pwd`
-BASE="/var/www"
-mkdir -p $BASE/www
+export DRUPAL_ROOT=/var/www/html
 DRUPAL=$DRUPAL
 NETICRM=$NETICRM
 DB="neticrmci"
 PW="123456"
-PORT=80
+RUNPORT=8080
 
 echo "export TERM=xterm" >> /root/.bashrc
 echo "export DRUPAL_ROOT=/var/www/html" >> /root/.bashrc
 echo "export CIVICRM_TEST_DSN=mysql://root@127.0.0.1/neticrmci" >> /root/.bashrc
-export DRUPAL_ROOT=/var/www/html
 export CIVICRM_TEST_DSN=mysql://root@127.0.0.1/neticrmci
 
 date +"@ %Y-%m-%d %H:%M:%S %z"
@@ -23,21 +21,18 @@ echo "CI for Drupal-$DRUPAL + netiCRM-$NETICRM"
 echo "Install new database $DB"
 mysql -uroot -e "CREATE DATABASE $DB CHARACTER SET utf8 COLLATE utf8_general_ci;"
 
-cd $BASE
+cd $DRUPAL_ROOT
 
 echo "Install Drupal ..."
 date +"@ %Y-%m-%d %H:%M:%S %z"
 sleep 5s
-php -d sendmail_path=`which true` ~/.composer/vendor/bin/drush.php --yes core-quick-drupal --core=drupal-$DRUPAL --no-server --db-url=mysql://root:@127.0.0.1/$DB --account-pass=$PW --site-name=netiCRM --enable=transliteration neticrmci
-
-mv $BASE/neticrmci/drupal-${DRUPAL}/* $DRUPAL_ROOT/
-mv $BASE/neticrmci/drupal-${DRUPAL}/.htaccess $DRUPAL_ROOT/
+php -d sendmail_path=`which true` ~/.composer/vendor/bin/drush.php --yes site-install standard --account-name=admin --db-url=mysql://root:@127.0.0.1/$DB --account-pass=$PW --site-name=netiCRM
 
 echo "Install netiCRM ..."
 cp -R $REPOSDIR $DRUPAL_ROOT/sites/all/modules/civicrm
 cd $DRUPAL_ROOT
-drush --yes pm-download simpletest
-drush --yes pm-enable civicrm simpletest
+drush --yes pm-enable transliteration simpletest
+drush --yes pm-enable civicrm
 drush --yes pm-enable civicrm_allpay civicrm_neweb civicrm_spgateway
 drush --yes variable-set civicrm_demo_sample_data 1
 drush --yes pm-enable civicrm_demo
@@ -46,6 +41,9 @@ drush --yes variable-set error_level 0
 chown -R www-data /var/www/html/sites/default/files
 echo 'date_default_timezone_set("Asia/Taipei");' >> $DRUPAL_ROOT/sites/default/settings.php
 echo 'ini_set("error_reporting", E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED & ~E_WARNING);' >> $DRUPAL_ROOT/sites/default/settings.php
+
+drush runserver 127.0.0.1:$RUNPORT >& /dev/null &
+until netstat -an 2>/dev/null | grep "${RUNPORT}.*LISTEN"; do true; done
 
 # testing...
 echo "Running test..."
