46ba2ff74d3ce024a2c45ccc2f98b7750198f9c6
Gogs <gogs@fake.local>
2018-03-17 03:27:55 +0800

    Use environment file for setting

--- drone-start.sh
+++ drone-start.sh
@@ -5,17 +5,14 @@ EXISTS=`docker ps -q -f name=drone`
 REALPATH=`realpath $0`
 WORKDIR=`dirname $REALPATH`
 
-if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
-  echo "Usage:"
-  echo "  $0 github_client_id github_secret organization"
-  exit 1;
+if [ -f "./drone/env" ]; then
+  source ./drone/env
+else
+  echo "No environmental file in drone/env"
+  exit 1
 fi
 
 if [ -z "$EXISTS" ]; then
-  export DRONE_GITHUB_CLIENT=$1
-  export DRONE_GITHUB_SECRET=$2
-  export DRONE_ORGS=$3
-  export DRONE_SECRET=NETivism
   cd $WORKDIR
   docker-compose up -d
 fi;
