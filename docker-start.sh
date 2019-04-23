5c45ed30f7b9af8d4db16c6ce89d62f24cc3e0a6
Jimmy Huang <jimmy@netivism.com.tw>
2019-04-23 15:02:34 +0800

    Change docker start parameter.

--- docker-start.sh
+++ docker-start.sh
@@ -12,10 +12,12 @@ docker run -d \
   -p 8888:80 \
   -v /etc/localtime:/etc/localtime:ro \
   -v $WORKDIR/container/init.sh:/init.sh \
+  -v /mnt/neticrm-7/civicrm:/mnt/neticrm-7/civicrm \
   -e "TZ=Asia/Taipei" \
   -e "RUNPORT=80" \
   -e "DRUPAL_ROOT=/var/www/html" \
   -e "CIVICRM_TEST_DSN=mysql://root@127.0.0.1/neticrmci" \
   -e "DRUPAL=$1" \
   -e "NETICRM=$2" \
-  netivism/neticrm-ci
+  netivism/neticrm-ci:drone
+docker exec neticrm-ci /init.sh
