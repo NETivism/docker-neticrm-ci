0d763611cfc6ba5a75ea62516f7d0e5cf6f465b4
Jimmy Huang <jimmy@netivism.com.tw>
2021-10-06 13:10:33 +0800

    Upgrade image to latest 5.6

--- container/init.sh
+++ container/init.sh
@@ -13,8 +13,8 @@ export RUNPORT=8080
 
 echo "export TERM=xterm" >> /root/.bashrc
 echo "export DRUPAL_ROOT=/var/www/html" >> /root/.bashrc
-echo "export CIVICRM_TEST_DSN=mysql://root@localhost/neticrmci" >> /root/.bashrc
-export CIVICRM_TEST_DSN=mysql://root@localhost/neticrmci
+echo "export CIVICRM_TEST_DSN=mysqli://root@localhost/neticrmci" >> /root/.bashrc
+export CIVICRM_TEST_DSN=mysqli://root@localhost/neticrmci
 
 date +"@ %Y-%m-%d %H:%M:%S %z"
 echo "CI for Drupal-$DRUPAL + netiCRM"
@@ -22,7 +22,7 @@ echo "CI for Drupal-$DRUPAL + netiCRM"
 EXISTSDB=`mysql -uroot -e "SHOW DATABASES" | grep neticrmci | wc -l`
 if [ "$EXISTSDB" = "0" ]; then
   echo "Install new database $DB"
-  mysql -uroot -e "CREATE DATABASE $DB CHARACTER SET utf8 COLLATE utf8_general_ci;"
+  mysql -uroot -e "CREATE DATABASE $DB CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;"
 fi
 
 cd $DRUPAL_ROOT
@@ -31,7 +31,7 @@ if [ ! -f $DRUPAL_ROOT/sites/defaut/settings.php ]; then
   echo "Install Drupal ..."
   date +"@ %Y-%m-%d %H:%M:%S %z"
   sleep 5s
-  php -d sendmail_path=`which true` ~/.composer/vendor/bin/drush.php --yes site-install standard --account-name=admin --db-url=mysql://root:@localhost/$DB --account-pass=$PW --site-name=netiCRM
+  php ~/.composer/vendor/bin/drush.php --yes site-install standard --account-name=admin --db-url=mysql://root:@localhost/$DB --account-pass=$PW --site-name=netiCRM
 
   echo "Install netiCRM ..."
   ln -s $REPOSDIR $DRUPAL_ROOT/sites/all/modules/civicrm
@@ -47,6 +47,7 @@ if [ ! -f $DRUPAL_ROOT/sites/defaut/settings.php ]; then
   chown -R www-data /var/www/html/sites/default/files
   echo 'date_default_timezone_set("Asia/Taipei");' >> $DRUPAL_ROOT/sites/default/settings.php
   echo 'ini_set("error_reporting", E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED & ~E_WARNING);' >> $DRUPAL_ROOT/sites/default/settings.php
+  echo "\$base_url='';" >> $DRUPAL_ROOT/sites/default/settings.php
 fi
 
 drush runserver --dns 127.0.0.1:$RUNPORT >& /dev/null &
