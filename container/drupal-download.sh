2f9700af9801c42c4dbc47057bead43dc3afcac9
Jimmy Huang <jimmy@netivism.com.tw>
2023-02-16 14:37:03 +0800

    Fixes various issues for ci

--- container/drupal-download.sh
+++ container/drupal-download.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
+if [ -z $1 ]; then
+  echo "Please specify drupal version prefix."
+  exit;
+fi
 VERSION_PREFIX=$1
 LATEST_VERSION=$(curl -s "https://www.drupal.org/node/3060/release/feed?version=$VERSION_PREFIX" | grep '<title>drupal' | head -1 | sed 's/[^0-9.]*//g' | tr -d '\n')
+
+if [ -d /tmp/drupal-${LATEST_VERSION} ]; then
+  echo "Directory /tmp/drupal-${LATEST_VERSION} exists. Delete directory for download newest file."
+  rm -Rf /tmp/drupal-${LATEST_VERSION}
+fi
 echo "Downloading Drupal $LATEST_VERSION ..."
 curl -s "https://ftp.drupal.org/files/projects/drupal-${LATEST_VERSION}.tar.gz" | tar -xz -C /tmp
-mv "/tmp/drupal-$LATEST_VERSION" /var/www/html
+if [ ! -d /var/www/html ]; then
+  mkdir -p /var/www/html
+fi
+mv /tmp/drupal-$LATEST_VERSION/* /tmp/drupal-$LATEST_VERSION/.[!.]* /var/www/html/
+rm -Rf /tmp/drupal-$LATEST_VERSION
+echo "Drupal $LATEST_VERSION downloaded on /var/www/html"
--- docker-start.sh
+++ docker-start.sh
@@ -8,6 +8,11 @@ if [ -z "$1" ]; then
   echo "  $0 0.0.0.0:8888"
   exit 1
 fi
+
+CONTAINER_NAME=neticrm-ci-php7
+TAG_NAME=drone-php7
+DRUPAL_VERSION=7
+
 WORKDIR=`pwd`
 if [ -d $HOME/mnt/neticrm-7/civicrm ];then
   MOUNT=$HOME/mnt/neticrm-7/civicrm
@@ -15,17 +20,35 @@ else
   MOUNT=/mnt/neticrm-7/civicrm
 fi
 
-#docker pull netivism/neticrm-ci:drone-php7
-docker rm -f neticrm-ci-php7
+# always fetch latest image
+docker pull ghcr.io/netivism/docker-neticrm-ci:$TAG_NAME
+
+# purge previous container
+EXISTS_CONTAINER=$(docker ps -q -f "name=$CONTAINER_NAME")
+if [ -n $EXISTS_CONTAINER ]; then
+  docker rm -f $CONTAINER_NAME
+  echo "Remove old container $CONTAINER_NAME successfully"
+fi
+
+# purge previous images
+OLD_IMAGE=$(docker images ghcr.io/netivism/docker-neticrm-ci --filter "dangling=true" -q)
+if [ -n "$OLD_IMAGE" ]; then
+  docker rmi $(docker images ghcr.io/netivism/docker-neticrm-ci --filter "dangling=true" -q)
+fi
+
+# start container
 docker run -d \
-  --name neticrm-ci-php7 \
+  --name $CONTAINER_NAME \
   -p $1:8080 \
   -v /etc/localtime:/etc/localtime:ro \
-  -v $WORKDIR/container/init.sh:/init.sh \
+  -v $WORKDIR/container/init-$DRUPAL_VERSION.sh:/init.sh \
   -v $MOUNT:/mnt/neticrm-7/civicrm \
   -e "TZ=Asia/Taipei" \
   -e "RUNPORT=8080" \
   -e "DRUPAL_ROOT=/var/www/html" \
   -e "CIVICRM_TEST_DSN=mysqli://root@localhost/neticrmci" \
-  netivism/neticrm-ci:drone-php7
-docker exec neticrm-ci-php7 /init.sh
+  -e "DRUPAL=$DRUPAL_VERSION" \
+  ghcr.io/netivism/docker-neticrm-ci:$TAG_NAME
+
+# install drupal
+docker exec $CONTAINER_NAME /init.sh
