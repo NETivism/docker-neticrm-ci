b39fc7580fd03bd058fb1af6f6d9106dd4e88dfa
Jimmy Huang <jimmy@netivism.com.tw>
2023-06-17 16:31:22 +0800

    Add correct permission for testing image

--- container/init-7.sh
+++ container/init-7.sh
@@ -45,6 +45,9 @@ if [ ! -f $DRUPAL_ROOT/sites/default/settings.php ]; then
   drush --yes pm-enable civicrm_demo
   drush --yes variable-set error_level 0
 
+  drush role-add-perm 1 'profile create,register for events,access CiviMail subscribe/unsubscribe pages,access all custom data,view event info,view public CiviMail content,make online contributions'
+  drush role-add-perm 2 'profile create,register for events,access CiviMail subscribe/unsubscribe pages,access all custom data,view event info,view public CiviMail content,make online contributions,profile edit'
+
   chown -R www-data /var/www/html/sites/default/files
   echo 'date_default_timezone_set("Asia/Taipei");' >> $DRUPAL_ROOT/sites/default/settings.php
   echo 'ini_set("error_reporting", E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED & ~E_WARNING);' >> $DRUPAL_ROOT/sites/default/settings.php
@@ -60,6 +63,8 @@ if [ -d $DRUPAL_ROOT/sites/all/modules/civicrm/tests/playwright ]; then
   cd $DRUPAL_ROOT/sites/all/modules/civicrm/tests/playwright 
   npm link @playwright/test
   npm link dotenv
+  pwd
+  echo -e "# .env file\nlocalUrl=http://127.0.0.1:$RUNPORT/\nadminUser=admin\nadminPwd=123456" >> $DRUPAL_ROOT/sites/all/modules/civicrm/tests/playwright/setup.env
 
   # install latest chromium
   npx playwright install --with-deps chromium
