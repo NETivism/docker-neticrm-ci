0f24f7aeed68b8a62f19c8ab35a8982d22a6fb2a
Jimmy Huang <jimmy@netivism.com.tw>
2023-08-25 10:29:44 +0800

    Add default environment for drupal version #skip-build

--- container/init-7.sh
+++ container/init-7.sh
@@ -15,6 +15,7 @@ echo "export TERM=xterm" >> /root/.bashrc
 echo "export DRUPAL_ROOT=/var/www/html" >> /root/.bashrc
 echo "export CIVICRM_TEST_DSN=mysqli://root@localhost/neticrmci" >> /root/.bashrc
 export CIVICRM_TEST_DSN=mysqli://root@localhost/neticrmci
+export DRUPAL=7
 
 date +"@ %Y-%m-%d %H:%M:%S %z"
 echo "CI for Drupal-$DRUPAL + netiCRM"
