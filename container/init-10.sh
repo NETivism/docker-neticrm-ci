3d1bb8d852b28b0d6c6b11442e12bf9f26f8bb1f
Jimmy Huang <jimmy@netivism.com.tw>
2026-02-07 16:34:55 +0800

    refs #45702, fixes drush command not in PATH

--- container/init-10.sh
+++ container/init-10.sh
@@ -32,7 +32,18 @@ cd $DRUPAL_ROOT
 if [ ! -f $DRUPAL_ROOT/sites/default/settings.php ]; then
   cd /var/www/html
   composer update "drupal/core-*" --with-all-dependencies
+
+  # correct drush installation
   composer require drush/drush
+
+  # add drush to PATH in bash.bashrc for future sessions
+  if ! grep -q "/var/www/html/vendor/bin" /etc/bash.bashrc; then
+    echo 'export PATH="/var/www/html/vendor/bin:$PATH"' >> /etc/bash.bashrc
+  fi
+
+  # add drush to PATH for current script execution
+  export PATH="/var/www/html/vendor/bin:$PATH"
+
   echo "Install Drupal ..."
   date +"@ %Y-%m-%d %H:%M:%S %z"
   sleep 5s
